1ed45941022c406677db08f0db7ae28610b663b9
paulsbpk <paulsobiepanek@gmail.com>
2025-03-31 09:13:00 +0200

    dez

--- cesi-site/Controllers/EntrepriseController.php
+++ cesi-site/Controllers/EntrepriseController.php
@@ -40,16 +40,28 @@ class EntrepriseController {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $nom = $_POST['nom'] ?? '';
             $description = $_POST['description'] ?? '';
-
+    
             if (!empty($nom) && !empty($description)) {
                 $this->entrepriseModel->createEntreprise($nom, $description);
-                header('Location: index.php?route=entreprises');
-                exit();
             }
         }
-
-        echo $this->twig->render('entreprise/index.html.twig');
+    
+        // Recharger les entreprises après l'ajout
+        $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+        $entreprisesParPage = 9;
+        $data = $this->entrepriseModel->getEntreprisesByPage($page, $entreprisesParPage);
+    
+        echo $this->twig->render('entreprise/index.html.twig', [
+            'entreprises' => $data['entreprises'],
+            'page_courante' => $data['page_courante'],
+            'total_pages' => $data['total_pages'],
+            'index_depart' => $data['index_depart'],
+            'entreprises_par_page' => $data['entreprises_par_page'],
+            'total_entreprises' => $data['total_entreprises']
+        ]);
     }
+    
 }
+
 
 ?>
